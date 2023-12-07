bbfc1d9287dbe8b43fa202c13d2eff094173c8ce
allan <allan.gtm92@gmail.com>
2023-12-07 08:25:44 +0545

    added home files

--- StepEleganceFrontEnd/src/App.tsx
+++ StepEleganceFrontEnd/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Login from "./pages/login.tsx";
 
+
+const router = createBrowserRouter(
+    [
+      {
+        path:"/",
+        element : <Login />
+      }
+    ]
+)
 function App() {
 
   return (
     <>
+      <RouterProvider router={router}></RouterProvider>
     </>
   )
 }
---
+++ StepEleganceFrontEnd/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import './home.css'
+
+
+function home(){
+    return (
+        <>
+
+        </>
+    )
+}
+export default home;
