39269f30766c082699cbb8f58409bb12e429c91f
tanish khoju <tkhoju@gmail.com>
2023-12-07 08:46:59 +0545

    added login html

--- StepEleganceFrontEnd/src/App.tsx
+++ StepEleganceFrontEnd/src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './App.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Login from './pages/login.tsx';
 
+
+
+
+const router=createBrowserRouter(
+    [
+      {
+        path:"/",
+        element:<Login/>
+      }
+    ]
+)
 function App() {
 
   return (
-    <>
-    </>
+      <>
+        <RouterProvider router={router} />
+
+      </>
   )
 }
-
 export default App
--- StepEleganceFrontEnd/src/pages/login.tsx
+++ StepEleganceFrontEnd/src/pages/login.tsx
@@ -2,9 +2,24 @@ import './login.css'
 function login(){
 
     return(
-        <>
+       <>
+           <head>
+               <title>Login Page</title>
+               <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+           </head>
+           <body>
+           <div className="SIGN-IN">
+               <h2>Sign-In Page</h2>
+               <label>
+                   Username: <input type="text" name="Username" />
+               </label>
+               <label>
+                   Password: <input type="Password" name="Password" />
+               </label>
+           </div>
+           </body>
+       </>
 
-        </>
     )
 }
 export default login
