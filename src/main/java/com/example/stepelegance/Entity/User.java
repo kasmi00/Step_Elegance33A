a21511a5ff9fc72bfdeb8b35dbfa9861f0e6c9f0
Allan0092 <112367617+Allan0092@users.noreply.github.com>
2023-12-13 16:26:22 +0545

    Modified user table according to E-R

--- src/main/java/com/example/stepelegance/Entity/User.java
+++ src/main/java/com/example/stepelegance/Entity/User.java
@@ -1,7 +1,11 @@
 package com.example.stepelegance.Entity;
 
+import com.example.stepelegance.Entity.UserDefinedDataEnums.Gender;
+import com.example.stepelegance.Entity.UserDefinedDataEnums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Builder
 @Getter
@@ -17,7 +21,10 @@ public class User {
     @Id
     @SequenceGenerator(name = "users_seq_gen", sequenceName = "users_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "users_seq_gen", strategy = GenerationType.SEQUENCE)
-    private Integer id;
+    private Integer userId;
+
+    @Column(name = "role", nullable = false)
+    private Role role;
 
     @Column(name = "firstName", nullable = false)
     private String firstName;
@@ -25,15 +32,21 @@ public class User {
     @Column(name="lastName", nullable = false)
     private String lastName;
 
-    @Column(name = "password", nullable = false)
-    private String password;
+    @Column(name="phone", nullable = true, unique = true)
+    private String phone;
 
     @Column(name="email", nullable = false)
     private String email;
 
+    @Column(name = "password", nullable = false)
+    private String password;
+
     @Column(name = "gender", nullable = false)
-    private String gender;
+    private Gender gender;
 
     @Column(name = "dateOfBirth", nullable = false)
-    private String dateOfBirth;
+    private LocalDate dateOfBirth;
+
+    @Column(name="token", nullable = false, unique = true)
+    private String token;
 }
--- src/main/java/com/example/stepelegance/dto/UserDTO.java
+++ src/main/java/com/example/stepelegance/dto/UserDTO.java
@@ -1,9 +1,13 @@
 package com.example.stepelegance.dto;
 
+import com.example.stepelegance.Entity.UserDefinedDataEnums.Gender;
+import com.example.stepelegance.Entity.UserDefinedDataEnums.Role;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -11,17 +15,23 @@ import lombok.NoArgsConstructor;
 public class UserDTO {
     private Integer id;
     @NotNull
+    private Role role;
+    @NotNull
     private String firstName;
     @NotNull
     private String lastName;
+    @NotNull
+    private String phone;
     @NotNull
     private String email;
     @NotNull
     private String password;
     @NotNull
-    private String dateOfBirth;
+    private Gender gender;
     @NotNull
-    private String Gender;
+    private LocalDate dateOfBirth;
+    @NotNull
+    private String token;
 
 
 
