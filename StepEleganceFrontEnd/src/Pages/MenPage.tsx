0a2232026da5036ef92c18e46888455377848e64
Suyogya17 <suyogyashrestha17@gmail.com>
2024-01-05 15:47:18 +0545

    naverror-correction

--- StepEleganceFrontEnd/src/Pages/MenPage.tsx
+++ StepEleganceFrontEnd/src/Pages/MenPage.tsx
@@ -12,8 +12,28 @@ import WFK from "../assets/Shoe/WFK.jpg";
 import yeezy from "../assets/Shoe/yeezy.jpg";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import footerComp from "./footerComp";
 
 function MenPage() {
+  enum ProductType{
+    SHOE = "SHOE",
+    ACCESSORIES = "ACCESSORIES"
+  }
+  enum ProductCategory{
+    MEN = "MEN",
+    WOMEN = "WOMEN",
+    KIDS = "KIDS"
+  }
+  interface ProductData{
+    productId: number;
+    productName: string;
+    description: string;
+    price: number;
+    quantity: number;
+    size: number;
+    type: ProductType;
+    category: ProductCategory;
+  }
   const { data } = useQuery({
     queryKey: ["GET_DATA"],
     queryFn() {
@@ -26,21 +46,17 @@ function MenPage() {
   return (
     <>
       <TopBar />
-
-      <div>
-        <div className="container">
-          <section className="container-card1">
-            {data?.data.map((i) => {
+        <div className="men-container">
+            {data?.data.map((i :ProductData) => {
               return (
                 <>
-                  <div className="card">
-                    <div className="image">
-                      <img src={Erke} />
+                  <div className="men-card">
+                    <div className="image-menshoe">
+                      <img src={Erke} alt="erick"/>
                     </div>
                     <div className="Title">
                       <h2>{i?.productName}</h2>
                     </div>
-
                     <p>{i?.description} </p>
                     <a href="">BUY NOW</a>
                   </div>
@@ -48,81 +64,9 @@ function MenPage() {
                 </>
               );
             })}
-            {/* <div className="card">
-            <div className="image">
-              <img src={Peak}/></div>
-            <h2>Peak 2741</h2>
-            <p>
-              High quality pure genuine leather
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-          <div className="card">t
-          <div className="image">
-              <img src={LS}/></div>
-            <h2>Lineseven 0035</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-          <div className="card">
-          <div className="image">
-              <img src={Fandei}/></div>
-            <h2>Fandei 227</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div> */}
-          </section>
-          {/* <section className="container-card2">
-          <div className="card">
-          <div className="image">
-              <img src={TW}/></div>
-            <h2>Warrior Trekking 537</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-          <div className="card">
-          <div className="image">
-              <img src={TW2}/></div>
-            <h2>Warrior Trekking 537</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-          <div className="card">
-          <div className="image">
-              <img src={WFK}/></div>
-            <h2>WFK 220</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-          <div className="card">
-          <div className="image">
-              <img src={yeezy}/></div>
-            <h2>Yeezy 888</h2>
-            <p>
-            High quality fibre material
-              Sizes available 39-43
-            </p>
-            <a href="">BUY NOW</a>
-          </div>
-        </section> */}
         </div>
-      </div>
+
+        <FooterComp />
     </>
   );
 }
